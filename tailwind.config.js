16994da43bc592ca09f4a5d1bbef438907589fb1
Iavor Dekov <idekov@heartflow.com>
2023-06-17 18:38:20 -0500

    Add tailwind, Foxglove image and logo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'fg-white': '#EEE6D4',
+        'fg-orange': '#DD6E39',
+        'fg-purple': '#923C8D',
+      }
+    },
+  },
+  plugins: [],
+}
+
