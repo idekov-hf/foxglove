2672b0626bc9f2fdb97e8d23af3b2b879ad7c58e
Iavor Dekov <idekov@heartflow.com>
2023-07-04 17:03:24 -0500

    Build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 // Import rollup plugins
-import html from '@web/rollup-plugin-html';
-import {copy} from '@web/rollup-plugin-copy';
-import resolve from '@rollup/plugin-node-resolve';
-import {terser} from '@rollup/plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-import summary from 'rollup-plugin-summary';
-
+import html from '@web/rollup-plugin-html'
+import { copy } from '@web/rollup-plugin-copy'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import minifyHTML from 'rollup-plugin-minify-html-literals'
+import summary from 'rollup-plugin-summary'
 
 export default {
 
@@ -13,7 +12,7 @@ export default {
     // Entry point for application build; can specify a glob to build multiple
     // HTML files for non-SPA app
     html({
-      input: 'index.html',
+      input: 'index.html'
     }),
     // Resolve bare module specifiers to relative paths
     resolve(),
@@ -23,18 +22,18 @@ export default {
     terser({
       ecma: 2020,
       module: true,
-      warnings: true,
+      warnings: true
     }),
     // Print bundle summary
     summary(),
     // Optional: copy any static assets to build directory
     copy({
-      patterns: ['images/**/*'],
-    }),
+      patterns: ['images/**/*']
+    })
   ],
   output: {
-    dir: 'build',
+    dir: 'build'
   },
-  preserveEntrySignatures: 'strict',
+  preserveEntrySignatures: 'strict'
 
-};
+}
