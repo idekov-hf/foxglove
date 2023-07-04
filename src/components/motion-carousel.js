0905fb02969ad0fbdceb0664c6b69f5818b9c124
Iavor Dekov <idekov@heartflow.com>
2023-07-04 16:47:34 -0500

    Add linting and create carousel

---
+++ src/components/motion-carousel.js
@@ -0,0 +1,63 @@
+import { LitElement, html } from 'lit'
+import { styles } from './motion-carousel-styles.js'
+
+export class MotionCarousel extends LitElement {
+  static properties = {
+    selected: { type: Number }
+  }
+
+  static styles = styles
+
+  constructor () {
+    super()
+    this.selected = 0
+  }
+
+  selectedInternal = 0
+
+  get maxSelected () {
+    return this.childElementCount - 1
+  }
+
+  hasValidSelected () {
+    return this.selected >= 0 && this.selected <= this.maxSelected
+  }
+
+  render () {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected
+    }
+    return html`
+      <div class="fit" @click=${this.clickHandler}>
+        <slot name="selected"></slot>
+      </div>
+    `
+  }
+
+  clickHandler (e) {
+    const i = this.selected + (Number(!e.shiftKey) || -1)
+    this.selected = i > this.maxSelected ? 0 : i < 0 ? this.maxSelected : i
+    const change = new CustomEvent('change', {
+      detail: this.selected,
+      bubbles: true,
+      composed: true
+    })
+    this.dispatchEvent(change)
+  }
+
+  previous = 0
+  updated (changedProperties) {
+    console.log(changedProperties)
+    if (changedProperties.has('selected') && this.hasValidSelected()) {
+      this.updateSlots()
+      this.previous = this.selected
+    }
+  }
+
+  updateSlots () {
+    this.children[this.previous]?.removeAttribute('slot')
+    this.children[this.selected]?.setAttribute('slot', 'selected')
+  }
+}
+
+customElements.define('motion-carousel', MotionCarousel)
